753c20e8b4c0d88d8839d204154e7411a518bef7
codayon <reshadahammedayon@proton.me>
2025-01-27 00:10:25 +0600

    little change two

---
+++ packages/packages.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+{
+  environment = {
+    systemPackages = with pkgs; [
+      gh
+      git
+      vim
+      btop
+      wget
+      unzip
+      kitty
+      ranger
+      neovim
+      haruna
+      yt-dlp
+      neofetch
+      chromium
+      vscodium
+      keepassxc
+      fastfetch
+      obs-studio
+      ffmpeg-full
+      nixpkgs-fmt
+      qbittorrent
+      protonvpn-gui
+      nixfmt-rfc-style
+    ];
+  };
+}
